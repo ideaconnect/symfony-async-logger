82eb8e47b25f6b5c884d0c50c4c56697f6b4da7c
IDCT Bartosz Pachołek <bartosz+github@idct.tech>
2024-09-12 02:39:14 +0200

    WIP added Symfoyn for testing

---
+++ tests/func/src/Command/TestCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Command;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:test',
+    description: 'Add a short description for your command',
+)]
+class TestCommand extends Command
+{
+    public function __construct(protected LoggerInterface $asyncStreamLogger)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $this->asyncStreamLogger->info('test123');
+
+        return Command::SUCCESS;
+    }
+}
